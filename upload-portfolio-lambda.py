f1f3a4655f997c0b15083e53b601d995f10ef224
Justin Wesley <justin@wesleyhome.com>
2018-06-21 21:39:52 -0500

    Guess mimetype

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,5 +1,7 @@
 import boto3
 import zipfile
+import mimetypes
+
 def lambda_handler(event, context):
     sns = boto3.resource('sns');
     name = 'portfolio.wesleyhome.com'
@@ -28,7 +30,7 @@ def lambda_handler(event, context):
         with zipfile.ZipFile('/tmp/portfoliobuild.zip') as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
         if job:
