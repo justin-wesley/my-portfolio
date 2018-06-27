d1ddcef7fcbef60b54f5958be9dd7184c14c40cd
Justin Wesley <justin@wesleyhome.com>
2018-06-27 10:41:20 -0500

    Added tests

--- js/example-work.js
+++ js/example-work.js
@@ -5,50 +5,41 @@ class ExampleWork extends React.Component {
     render() {
         return (
             <section className="section section--alignCentered section--description">
-                <div className="section__exampleWrapper">
-                    <div className="section__example">
-                        <img alt="example screenshot of a project involving code"
-                             className="section__exampleImage"
-                             src="images/example1.png"/>
-                        <dl className="color--cloud">
-                            <dt className="section__exampleTitle section__text--centered">
-                                Work Example
-                            </dt>
-                            <dd></dd>
-                        </dl>
-                    </div>
-                </div>
-
-                <div className="section__exampleWrapper">
-                    <div className="section__example">
-                        <img alt="example screenshot of a project involving chemistry"
-                             className="section__exampleImage"
-                             src="images/example2.png"/>
-                        <dl className="color--cloud">
-                            <dt className="section__exampleTitle section__text--centered ">
-                                Work Example
-                            </dt>
-                            <dd></dd>
-                        </dl>
-                    </div>
-                </div>
-
-                <div className="section__exampleWrapper">
-                    <div className="section__example">
-                        <img alt="example screenshot of a project involving cats"
-                             className="section__exampleImage"
-                             src="images/example3.png"/>
-                        <dl className="color--cloud">
-                            <dt className="section__exampleTitle section__text--centered">
-                                Work Example
-                            </dt>
-                            <dd></dd>
-                        </dl>
-                    </div>
-                </div>
+                {
+                    this.props.work.map((example, index) => {
+                        return (
+                            <ExampleWorkBubble example={example} key={index}/>
+                        );
+                    })
+                }
             </section>
         )
     }
 }
 
+class ExampleWorkBubble extends React.Component {
+
+    render() {
+        let example = this.props.example;
+        return (
+            <div className="section__exampleWrapper">
+                <div className="section__example">
+                    <img alt={example.image.description}
+                         className="section__exampleImage"
+                         src={example.image.src}/>
+                    <dl className="color--cloud">
+                        <dt className="section__exampleTitle section__text--centered">
+                            {example.title}
+                        </dt>
+                        <dd></dd>
+                    </dl>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
 export default ExampleWork;
+export {ExampleWorkBubble};
--- js/main.js
+++ js/main.js
@@ -2,4 +2,26 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import ExampleWork from './example-work';
 
-ReactDom.render(<ExampleWork/>, document.getElementById('exampleWork'))
+const myWork = [
+    {
+        "title": "Work Example",
+        "image": {
+            "description": "example screenshot of a project involving code",
+            "src": "images/example1.png"
+        }
+    }, {
+        "title": "Work Example",
+        "image": {
+            "description": "example screenshot of a project involving chemistry",
+            "src": "images/example2.png"
+        }
+    }, {
+        "title": "Work Example",
+        "image": {
+            "description": "example screenshot of a project involving cats",
+            "src": "images/example3.png"
+        }
+    }
+
+]
+ReactDom.render(<ExampleWork work={myWork}/>, document.getElementById('exampleWork'))
